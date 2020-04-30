9a3413eb1f22fa13d82883691ccc527669b7fc17
Erin Mettille <ecmettille@gmail.com>
2020-04-30 03:08:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods, Findable
+  include Paramble
   attr_accessor :name
   attr_reader :artist
 
@@ -9,10 +10,6 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
